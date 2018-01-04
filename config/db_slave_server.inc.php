c826658a66278042c67884d90e259e12336f250a
Tim Izzo <sika.tim@gmail.com>
2018-01-04 10:09:37 +0100

    Remove useless in manifest

--- config/db_slave_server.inc.php
+++ config/db_slave_server.inc.php
@@ -2,5 +2,5 @@
 
 return array(
     array('server' => 'DB_1', 'user' => 'DBUSER', 'password' => 'DBPASS', 'database' => 'prestashop'),
-    array('server' => 'DB_2', 'user' => 'DBUSER', 'password' => 'DBPASS', 'database' => 'prestashop'),
+    array('server' => 'DB_2', 'user' => 'DBUSER', 'password' => 'DBPASS', 'database' => 'prestashop')
     );
