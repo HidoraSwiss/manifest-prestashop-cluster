9d8c0352f22c2d84bcb4addaace2c76efaff7608
Tim Izzo <sika.tim@gmail.com>
2018-01-04 09:43:43 +0100

    Config db slaves

---
+++ config/db_slave_server.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    array('server' => 'DB_1', 'user' => 'DBUSER', 'password' => 'DBPASS', 'database' => 'prestashop'),
+    array('server' => 'DB_2', 'user' => 'DBUSER', 'password' => 'DBPASS', 'database' => 'prestashop'),
+    );
